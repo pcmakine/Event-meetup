98a9b331cb92d41302fbfd07f1746f791e332b8e
Pete <pmmakinen@hotmail.com>
2015-05-29 01:28:08 +0300

    going to the root redirects to registrations now

---
+++ src/main/java/wad/controller/DefaultController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wad.controller;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Pete
+ */
+@Controller
+@RequestMapping("*")
+public class DefaultController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String view(Model model) {
+        return "redirect:/registrations";
+    }
+
+}
