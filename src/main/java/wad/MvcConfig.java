f29030240f902cf137f7c9c4b1a896dbc2d7b1e8
Pete <pmmakinen@hotmail.com>
2015-06-05 22:01:50 +0300

    bootstrap dependency added

---
+++ src/main/java/wad/MvcConfig.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wad;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author Pete
+ */
+@EnableWebMvc
+@Configuration
+@ComponentScan
+public class MvcConfig extends WebMvcConfigurerAdapter {
+ 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+ 
+}
