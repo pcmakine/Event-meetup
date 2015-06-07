d07724287f1f89ef0d79c617f8b9b4eab1201783
Pete <pmmakinen@hotmail.com>
2015-06-07 16:23:48 +0300

    trying to make registrations work

--- src/main/java/wad/controller/RegistrationController.java
+++ src/main/java/wad/controller/RegistrationController.java
@@ -2,6 +2,7 @@ package wad.controller;
 
 import java.util.Date;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,7 +11,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import wad.domain.Event;
 import wad.domain.Registration;
+import wad.repository.EventRepository;
 import wad.repository.RegistrationRepository;
 
 @Controller
@@ -18,6 +22,8 @@ public class RegistrationController {
 
     @Autowired
     private RegistrationRepository registrationRepository;
+    @Autowired
+    private EventRepository eventRepository;
 
     @ModelAttribute("registration")
     private Registration getRegistration() {
@@ -39,8 +45,15 @@ public class RegistrationController {
     @RequestMapping(value = "/registrations", method = RequestMethod.POST)
     public String register(
             @Valid @ModelAttribute("registration") Registration registration,
-            BindingResult bindingResult) {
+            BindingResult bindingResult, @RequestParam String eventId, HttpServletRequest request) {
         if (!bindingResult.hasErrors()) {
+            Event event = eventRepository.findOne(Long.valueOf(request.getParameter("eventId")));
+            List<Registration> registrations = event.getRegistrations();
+            registrations.add(registration);
+            event.setRegistrations(registrations);
+            eventRepository.save(event);
+            registration.setEvent(event);
+            
             registration.setRegistrationDate(new Date());
             registrationRepository.save(registration);
             return "redirect:success.html";
